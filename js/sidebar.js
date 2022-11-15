e1660c1ab063b4f0438aac51b837e24ec2140b73
evaaldo <evaaldormj@gmail.com>
2022-11-15 20:35:47 -0300

    Finalização da aba cabeçalho no mobile

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,18 @@
 const sidebar = document.querySelector('[data-side-bar]')
+const lista = document.querySelector('[data-lista-mobile]')
+
+sidebar.addEventListener('click', function()  {
+
+    showHide()
+
+})
+
+function showHide() {
+
+    if(lista.style.display === 'none') {
+        lista.style.display = 'flex'
+    } else {
+        lista.style.display = 'none'
+    }
+
+}
