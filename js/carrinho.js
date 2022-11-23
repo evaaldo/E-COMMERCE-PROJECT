e13d5aad430a002f3b989228736e08845de1451e
evaaldo <evaaldormj@gmail.com>
2022-11-23 15:34:37 -0300

    Criação da funcionalidade do  botão de comprar e correação das imagens

--- js/carrinho.js
+++ js/carrinho.js
@@ -3,6 +3,10 @@ const closeModal = document.querySelector('[data-close]')
 const modal = document.querySelector('[data-modal]')
 const fade = document.querySelector('[data-fade]')
 
+const quantidade = document.getElementById('compre__conosco-carrinho')
+const mensagemCompra = document.getElementById('mensagem-compra')
+const adicionarCarrinho = document.getElementById('adicionar-carrinho')
+
 const toggleModal = () => {
 
     [modal, fade].forEach((el) => el.classList.toggle('hides'))
@@ -14,3 +18,12 @@ const toggleModal = () => {
 
 })
 
+adicionarCarrinho.addEventListener('click', () => {
+
+    mensagemCompra.innerHTML = `
+    <a id="mensagem-compra" href="https://wa.me/5585998500745?text=Olá, eu gostaria de comprar ${quantidade.value} polaroids de" target="_blank">
+        <button class="compre-conosco__adicionar comprar"><i class="fa-solid fa-sack-dollar"></i> Comprar</button>
+    </a>
+    `
+
+})
